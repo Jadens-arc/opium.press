5b0032db4ed57d75ff840b5cf300bc6c9a302719
Jaden Arceneaux <arceneauxjaden@gmail.com>
2023-04-03 21:40:14 -0700

    writer page changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,39 +52,26 @@ class RegistrationController extends AbstractController
 
     #[Route('/writer/edit', name: 'app_edit_account')]
     public function edit(Request $request,
-                         UserPasswordHasherInterface $userPasswordHasher,
                          EntityManagerInterface $entityManager,
                          UserInterface $user) {
-        $form = $this->createForm(EditUserFormType::class, $user);
 
-        $form->handleRequest($request);
+        $form = $this->createForm(EditUserFormType::class);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->get('newPassword')->getData()) {
-//                $metadata->addPropertyConstraint(
-//                    $form->get('plainPassword')->getData(),
-//                    new SecurityAssert\UserPassword([
-//                        'message' => 'Wrong value for your current password',
-//                    ])
-//                );
+        $form->handleRequest($request, $user);
 
-                $user->setPassword(
-                    $userPasswordHasher->hashPassword(
-                        $user,
-                        $form->get('newPassword')->getData()
-                    )
-                );
-            }
-
+        if ($form->isSubmitted()) {
+            $user->setEmail($form['email']->getData());
+            $user->setDisplayName($form['display_name']->getData());
+            $user->setBio($form['bio']->getData());
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_homepage');
+            return $this->redirectToRoute('app_writer', ['username' => $user->getUsername()]);
         }
 
-        return $this->render('registration/edit.html.twig', [
-            'registrationForm' => $form->createView(),
+        return $this->renderForm('registration/edit.html.twig', [
+            'form' => $form,
+            'user' => $user
         ]);
     }
 }
--- src/Form/EditUserFormType.php
+++ src/Form/EditUserFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -19,7 +21,8 @@ class EditUserFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class)
-            ->add('display_name')
+            ->add('display_name', TextType::class)
+            ->add('bio', TextareaType::class)
         ;
     }
 
