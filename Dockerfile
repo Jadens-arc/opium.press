8c7b2972b5da1365a6c2d4ea5ee990140ef277ea
Jaden Arceneaux <contact@jadenarceneaux.com>
2025-04-01 17:25:32 -0700

    fixed images

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt-get update && apt-get install -y \
     libzip-dev \
     libpng-dev \
     libjpeg-dev \
+    freetype* \
     libfreetype6-dev \
     libonig-dev \
     libxml2-dev \
@@ -14,7 +15,8 @@ RUN apt-get update && apt-get install -y \
     curl
 
 # Install PHP extensions
-RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
+RUN docker-php-ext-configure gd --with-freetype --with-jpeg && \
+    docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
 
 # Set working directory to /app
 WORKDIR /app
--- app/src/Controller/HomepageController.php
+++ app/src/Controller/HomepageController.php
@@ -13,52 +13,57 @@ use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 
-
-
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route("/", name: "app_homepage")]
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $currentDate = new DateTime();
         $currentDate->modify("-3 day");
-        $currentDate = $currentDate->format('Y-m-d H:i:s');
+        $currentDate = $currentDate->format("Y-m-d H:i:s");
 
-        $searchQuery = $request->query->get('q');
+        $searchQuery = $request->query->get("q");
 
+        $qb = $doctrine->getRepository(Post::class)->createQueryBuilder("p");
 
-        $qb = $doctrine->getRepository(Post::class)
-            ->createQueryBuilder('p');
-
-        if ($searchQuery) { // if user is searching
+        if ($searchQuery) {
+            // if user is searching
             $rawSearch = substr($searchQuery, 1); // remove @ and #
-            if (strpos($searchQuery, "#") !== false) { // searching for hashtags
-                $qb->where("p.tags like :search")
-                    ->setParameter('search', "%".$rawSearch."%");
-            } else { // just searching by content and title
+            if (strpos($searchQuery, "#") !== false) {
+                // searching for hashtags
+                $qb->where("p.tags like :search")->setParameter(
+                    "search",
+                    "%" . $rawSearch . "%"
+                );
+            } else {
+                // just searching by content and title
                 $qb->where("p.content like :search")
                     ->orWhere("p.title like :search")
-                    ->setParameter('search', "%".$searchQuery."%");
+                    ->setParameter("search", "%" . $searchQuery . "%");
             }
         }
 
-        $qb->andWhere('p.creationDate < :currentDate')
-            ->setParameter('currentDate', $currentDate)
-            ->orderBy('p.creationDate', 'DESC')
-        ;
+        $qb->andWhere("p.creationDate < :currentDate")
+            ->setParameter("currentDate", $currentDate)
+            ->orderBy("p.creationDate", "DESC");
 
         $adapter = new QueryAdapter($qb);
         $pagerfanta = new Pagerfanta($adapter);
 
-        $title =  "Latest Capsules... ";
-        if ($searchQuery) $title = "Results for \"$searchQuery\"";
+        $title = "Latest Capsules... ";
+        if ($searchQuery) {
+            $title = "Results for \"$searchQuery\"";
+        }
 
         if (isset($_GET["page"])) {
             $pagerfanta->setCurrentPage($_GET["page"]);
             $title = " Page " . $_GET["page"] . " of " . $title;
         }
 
-        return $this->render('homepage/index.html.twig', ["title" => $title,  "posts"=>$pagerfanta->getCurrentPageResults(), 'pager' => $pagerfanta]);
+        return $this->render("homepage/index.html.twig", [
+            "title" => $title,
+            "posts" => $pagerfanta->getCurrentPageResults(),
+            "pager" => $pagerfanta,
+        ]);
     }
-
 }
